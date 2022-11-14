04071ba0e7f3dd1e1ddd0f564a18e03966320b80
Dileep Sai Vara Prasad Bhupa <dileep.bhupa@AD.MSYSTECHNOLOGIES.COM>
2022-11-14 17:39:44 +0530

    form section data added

---
+++ src/Constants/FormSectionsData/FormSectionsData.js
@@ -0,0 +1,35 @@
+export const states = [
+    'Select State',
+    'Andhra Pradesh',
+    'Arunachal Pradesh',
+    'Assam',
+    'Banglore',
+    'Bihar',
+    'Chennai',
+    'Delhi',
+    'Hyderabad',
+    'Karnataka',
+    'Kolkata',
+    'Maharastra',
+    'Pune'
+]
+
+export const admins = [
+    'Select Admin',
+    'Kishore',
+    'Shilpa',
+    'Ganesh',
+    'Sasi'
+]
+
+export const domains  = [
+    'Services',
+    'IT Services',
+    'Finance Service',
+    'Online Booking',
+    'Ecommerce',
+    'Hospitality',
+    'Health Care',
+    'Construction Services',
+    'Automobiles'
+]
